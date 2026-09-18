43a3e67e3ca9e3257efa5c31f170f31d24332961
sashadlp <stitchedlp@gmail.com>
2026-09-18 10:24:27 +0200

    js affichage pokemon

--- pokedex.js
+++ pokedex.js
@@ -44,7 +44,7 @@
 
 console.log(allPokemon);
     
-    console.log(allPokemon)
+    afficherPokemon(allPokemon);
   }
   else{
     // Status code >= 300
@@ -76,3 +76,19 @@ document.getElementById('searchBtn').addEventListener('click', () => {
   
   afficherPokemon(resultats);
 });
+function afficherPokemon(liste) {
+  const grid = document.getElementById('pokemon-grid');
+  grid.innerHTML = "";
+  liste.forEach(p => {
+    const div = document.createElement("div");
+     div.classList.add("pokemon-card");
+
+     div.innerHTML = `
+        <span class="badge-id">#${p.id}</span>
+        <img src="${p.sprites}" alt="${p.nom}">
+        <h3>${p.nom}</h3>
+        <button class="btn-favori" data-id="${p.id}">⭐ Favori</button>
+            `;
+            grid.appendChild(div);
+  });
+}
